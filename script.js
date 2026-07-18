23d1e41d51af4d50792f73ae358519eebce09df0
jcsjulio <wjcsjulio@gmail.com>
2026-07-18 09:14:11 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const lista = [
+    "11111111111",
+    "22222222222",
+    "33333333333",
+    "44444444444",
+    "55555555555",
+    "66666666666"
+]
+
+var n=0
+
+function funcao(){
+
+   
+  
+    resposta.innerHTML  = lista[n]
+    n = Math.floor(Math.random() * 6);
+    
+}
